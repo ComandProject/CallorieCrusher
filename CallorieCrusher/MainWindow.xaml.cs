afe2179b9abad70b77235a940f29f8e2177dc0d6
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-04-14 10:35:02 +0300

    Зробив дизайн головного меню

--- CallorieCrusher/MainWindow.xaml.cs
+++ CallorieCrusher/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CallorieCrusher
 {
@@ -23,8 +24,47 @@ namespace CallorieCrusher
         public MainWindow()
         {
             InitializeComponent();
-
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            Time.Content = $"{dateTime.ToLongDateString()}    {dateTime.ToShortTimeString()}";
         }
 
+        private void Main_Click(object sender, RoutedEventArgs e)
+        {
+            Page1.Visibility=Visibility.Visible;
+            Page2.Visibility = Visibility.Hidden;
+            Page3.Visibility = Visibility.Hidden;
+            Page4.Visibility = Visibility.Hidden;
+        }
+
+        private void Eat_Click(object sender, RoutedEventArgs e)
+        {
+            Page1.Visibility = Visibility.Hidden;
+            Page2.Visibility = Visibility.Visible;
+            Page3.Visibility = Visibility.Hidden;
+            Page4.Visibility = Visibility.Hidden;
+        }
+
+        private void Calend_Click(object sender, RoutedEventArgs e)
+        {
+            Page1.Visibility = Visibility.Hidden;
+            Page2.Visibility = Visibility.Hidden;
+            Page3.Visibility = Visibility.Visible;
+            Page4.Visibility = Visibility.Hidden;
+        }
+
+        private void Stat_Click(object sender, RoutedEventArgs e)
+        {
+            Page1.Visibility = Visibility.Hidden;
+            Page2.Visibility = Visibility.Hidden;
+            Page3.Visibility = Visibility.Hidden;
+            Page4.Visibility = Visibility.Visible;
+        }
     }
 }
