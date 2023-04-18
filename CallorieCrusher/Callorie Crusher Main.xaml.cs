956abc102aaaaab0a5ab55a3c54c36ae0b1a5d1a
PieceOfEon <danilskripka1996@gmail.com>
2023-04-18 21:02:34 +0300

    Radomir+Dima+Danil=USPEH

--- CallorieCrusher/Callorie Crusher Main.xaml.cs
+++ CallorieCrusher/Callorie Crusher Main.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +30,7 @@ namespace CallorieCrusher
             timer.Interval = TimeSpan.FromSeconds(0.2);
             timer.Tick += timer_Tick;
             timer.Start();
+            filling();
         }
         private void timer_Tick(object sender, EventArgs e)
         {
@@ -75,6 +79,57 @@ namespace CallorieCrusher
             WindowAddFood windowAddFood = new WindowAddFood();
             windowAddFood.ShowDialog();
         }
+    
+
+    public class FoodClass
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string picture { get; set; }
+        public string about { get; set; }
+        public float bilok { get; set; }
+        public float zhirok { get; set; }
+        public float uglevodi { get; set; }
+        public float cal { get; set; }
+        public float water { get; set; } 
     }
 
+        public ObservableCollection<FoodClass> Foods { get; set; }
+        private async void filling()
+        {
+            Foods = new ObservableCollection<FoodClass>();
+            string connectionString = @"Data Source = DESKTOP-JA41I9L; Initial Catalog = CalCrush; Trusted_Connection=True";
+            string catall = "SELECT * FROM Food";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                SqlCommand command = new SqlCommand(catall, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+
+                if (reader.HasRows)
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        object id_ = reader.GetValue(0);
+                        object name_ = reader.GetValue(1);
+                        object picture_ = reader.GetValue(2);
+                        object about_ = reader.GetValue(3);
+                        object bilok_ = reader.GetValue(4);
+                        object zhirok_ = reader.GetValue(5);
+                        object uglevodi_ = reader.GetValue(6);
+                        object cal_ = reader.GetValue(7);
+                        object water_ = reader.GetValue(8);
+                        Foods.Add(new FoodClass() { id = Convert.ToInt32(id_), name = name_.ToString(), about = about_.ToString(), bilok = float.Parse(bilok_.ToString(), CultureInfo.InvariantCulture.NumberFormat), zhirok = float.Parse(zhirok_.ToString(), CultureInfo.InvariantCulture.NumberFormat), uglevodi = float.Parse(uglevodi_.ToString(), CultureInfo.InvariantCulture.NumberFormat), cal = float.Parse(cal_.ToString(), CultureInfo.InvariantCulture.NumberFormat), water = float.Parse(water_.ToString(), CultureInfo.InvariantCulture.NumberFormat) });
+
+                    }
+                    reader.Close();
+                }
+                FoodList.ItemsSource = Foods;
+            }
+
+        }
+    }
+   
 }
